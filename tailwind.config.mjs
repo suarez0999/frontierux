1cad3e0783e3f53da17f906b8a89b18f71035337
suarez0999 <diegos6154830@gmail.com>
2025-01-24 18:58:47 -0500

    Remove unused pages and layouts; add new DailyUI layout and ticket generator page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        OpenSans: ["OpenSansFont", "sans-serif", "system-ui"],
+        Monserrat: ["MonserratFont", "sans-serif", "system-ui"],
+        Poppins: ["PoppinsFont", "sans-serif", "system-ui"],
+      },
+    },
+  },
+  plugins: [],
+};
